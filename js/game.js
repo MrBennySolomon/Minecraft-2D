e14879a6c31fb04ad4ae0adb29c492416a1d5755
Benny Solomon <mr.benny.solomon@gmail.com>
2023-02-02 23:36:21 +0200

    game building ver 2.1

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+
 const board = [
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
@@ -6,13 +7,13 @@ const board = [
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [6, 6, 6, 6, 6, 6, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
   [6, 6, 6, 6, 7, 7, 7, 7, 6, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-  [6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+  [6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 6, 6],
   [6, 6, 6, 6, 6, 6, 6, 7, 7, 6, 6, 6, 6, 6, 6, 5, 5, 5, 6, 6],
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 6, 6],
   [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 6, 6],
-  [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6],
-  [6, 6, 6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 6, 6, 6],
-  [6, 6, 6, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 4, 4, 6, 5, 6, 6, 4],
+  [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 6, 6, 6],
+  [6, 6, 6, 6, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 3, 6, 6, 6],
+  [6, 6, 6, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 4, 4, 6, 3, 6, 6, 4],
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -20,8 +21,42 @@ const board = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
-
+const selectedTool    = '';
+const axe             = document.querySelector('.axe');
+const shovel          = document.querySelector('.shovel');
+const pickaxe         = document.querySelector('.pickaxe');
 const gameBoard       = document.querySelector('body .game #game-board');
+const lastPickedFrame = document.querySelector('.last-picked');
+const tilesPicked     = [];
+const btnReset        = document.querySelector('.reset');
+
+btnReset.addEventListener('click', (e) => {
+  while (gameBoard.hasChildNodes()) {
+    gameBoard.removeChild(gameBoard.firstChild);
+  }
+
+  buildBoard(board);
+})
+axe.addEventListener('click', function(e) {
+  pickaxe.classList.remove('selected');
+  shovel.classList.remove('selected');
+  axe.classList.add('selected');
+  selectedTool = 'axe';
+});
+shovel.addEventListener('click', function(e) {
+  pickaxe.classList.remove('selected');
+  axe.classList.remove('selected');
+  shovel.classList.add('selected');
+  selectedTool = 'shovel';
+});
+pickaxe.addEventListener('click', function(e) {
+  axe.classList.remove('selected');
+  shovel.classList.remove('selected');
+  pickaxe.classList.add('selected');
+  selectedTool = 'pickaxe';
+  
+});
+
 
 const buildBoard = (board) => {
   for (let i = 0; i < board.length; i++) {
@@ -30,24 +65,31 @@ const buildBoard = (board) => {
       switch (board[i][j]) {
         case 1:
           cell.classList.add('dirt');
+          cell.setAttribute('tool', 'shovel');
           break;
         case 2:
           cell.classList.add('grass');
+          cell.setAttribute('tool', 'shovel');
           break;
         case 3:
-          cell.classList.add('leaves');
+          cell.classList.add('wood');
+          cell.setAttribute('tool', 'axe');
           break;
         case 4:
           cell.classList.add('stone');
+          cell.setAttribute('tool', 'pickaxe');
           break;
         case 5:
-          cell.classList.add('wood');
+          cell.classList.add('leaves');
+          cell.setAttribute('tool', 'shovel');
           break;
         case 6:
           cell.classList.add('sky');
+          cell.setAttribute('tool', 'none');
           break;
         case 7:
           cell.classList.add('cloud');
+          cell.setAttribute('tool', 'none');
           break;
         default:
           throw new Error('NON RECOGNIZED NUMBER');
@@ -59,7 +101,9 @@ const buildBoard = (board) => {
 }
 
 gameBoard.addEventListener('click', function(event) {
-  event.target.classList.add('none');
+  tilesPicked.push(event.target);
+  lastPickedFrame.classList.add(tilesPicked[tilesPicked.length - 1].getAttribute('class'));
+  event.target.setAttribute('class', 'none');
 });
 
 buildBoard(board);
